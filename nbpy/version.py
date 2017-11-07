49ff75baa9be1874c3dbcb1409fbfeac38741cbe
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-11-07 23:01:07 +0100

    v0.1.1

--- nbpy/version.py
+++ nbpy/version.py
@@ -1,6 +1,6 @@
 """Metadata for NBPy package."""
 
-version = "0.1.0"
+version = "0.1.1"
 title = "NBPy"
 description = "A utility package for calling NBP (Polish National Bank) Web API."
 author = "Piotr Kuszaj"
