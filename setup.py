9a225d3c71a31876781505ada4b0c543e30a67a4
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-11-07 22:02:17 +0100

    Added long_description (from README.rst)

--- setup.py
+++ setup.py
@@ -12,11 +12,15 @@ from nbpy.version import (
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
+# Long description (from README.rst)
+with open('README.rst', 'r') as readme:
+    long_description = readme.read()
 
 setup(
     name=title,
     version=version,
     description=description,
+    long_description=long_description,
     author=author,
     author_email=author_email,
     license=license,
